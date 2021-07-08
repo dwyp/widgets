ced90d244951a6fa5f37416139f29620d3eebc39
daniel.wypiszynski <daniel.wypiszynski@cardinalhealth.com>
2021-07-08 11:41:25 -0400

    Rework problem statement

--- src/main/java/com/kata/practice/PracticeApplication.java
+++ src/main/java/com/kata/practice/PracticeApplication.java
@@ -3,8 +3,8 @@ package com.kata.practice;
 /**
 
 For this exercise, you are a distributor who needs to order and deliver products to a customer on a specific date.
-You use just-in-time ordering to minimize warehousing costs. It takes one day for you to provide product from your
-warehouse to your customer.
+ 1. You use just-in-time ordering to minimize warehousing costs, so you want to order it as late as possible.
+ 2. It takes one day for you to provide product from your warehouse to your customer.
 
 Manufacturer A sells blue widgets and delivers within two to three business days. Per your contract with this
 manufacturer, you can only order on Tuesdays.
@@ -18,13 +18,13 @@ manufacturer so that you can order from them every day.
 Scenarios
 
 Today is Monday, February 3rd, 2020. As a customer, I order three blue widgets to gift them on Valentine's Day.
-What day does the distributor need to place the order?
+What day do you as the distributor need to place the order?
 
 Today is Monday, February 4th, 2019. As a customer, I order three blue widgets and two green widgets to gift them on
-Valentine's Day. What day does distributor need to place the order?
+Valentine's Day. What day do you as the distributor need to place the order?
 
 Today is Monday, February 1st, 2021. As a customer, I order three blue widgets, two green widgets, and one red widget
-to gift them on Valentine's Day. What day does distributor need to place the order?
+to gift them on Valentine's Day. What day do you as the distributor need to place the order?
 
  **/
 
